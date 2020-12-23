49c5d0390d02cee59f4f5740e204ebed2832c795
oscarjsv <gumball_junior@hotmail.com>
2020-12-22 20:15:12 -0500

    conver webstatic in file tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,16 @@
-'''Fabric script'''
+#!/usr/bin/python3
+# Write a Fabric script that generates a .tgz archive
 
-from fabric.api import *
 from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
-    '''do pack module'''
+    ''' do pack module '''
 
-    file_name = "web_static_" + \
+    file_name = 'web_static' +\
         datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
-    save_dir = "versions/"
+    save_dir = 'versions/'
 
     local("mkdir -p " + save_dir)
     chk = local("tar -cvzf {}{} web_static".format(save_dir, file_name))
