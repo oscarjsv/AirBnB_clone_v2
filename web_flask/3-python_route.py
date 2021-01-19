9dacccc27e6810c51abfa361cefcc477f753536f
oscarjsv <gumball_junior@hotmail.com>
2021-01-18 20:40:22 -0500

    task 1, 5

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' flask comment'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hellohbnb():
+    ''' module flask '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' module flask '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C " + text.replace('_', " ")
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def p(text):
+    return "Python " + text.replace('_', " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+''' flask comment'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hellohbnb():
+    ''' module flask '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' module flask '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C " + text.replace('_', " ")
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def p(text):
+    return "Python " + text.replace('_', " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    return"{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
