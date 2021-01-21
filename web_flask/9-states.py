8491ea252ff5a085d2d8633926d29d89132a81de
oscarjsv <gumball_junior@hotmail.com>
2021-01-20 21:13:20 -0500

    9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
+    """Route states_list"""
+    if id:
+        id = "State.{}".format(id)
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template('9-states.html',
+                           states=states, cities=cities, id=id)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """Close db session"""
+    storage.close()
+
+
+if __name__ == '__main__':
